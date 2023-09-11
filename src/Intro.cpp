34117a18a4dabccb2cde67854109de6f35641cce
Allan Legemaate <alegemaate@gmail.com>
2023-09-10 21:40:22 -0400

    chore: upgrade asw

--- src/Intro.cpp
+++ src/Intro.cpp
@@ -5,11 +5,11 @@
 
 void Intro::init() {
   intro = asw::assets::loadTexture("assets/img/intro.png");
-  timer.Start();
+  timer.start();
 }
 
 void Intro::update() {
-  auto time = timer.GetElapsedTime<milliseconds>();
+  auto time = timer.getElapsedTime<std::chrono::milliseconds>();
 
   if (time >= 3000 || asw::input::keyboard.anyPressed) {
     setNextState(ProgramState::STATE_MENU);
